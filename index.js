715aa744bfe34c5868beb36b462203b649e8a439
nikita2502 <nikitatiwarijavarnd@gmail.com>
2019-05-29 15:56:12 -0400

    2 files modified

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 import 'react-bootstrap';
+import { table } from 'react-bootstrap';
 
 class App extends Component {
   constructor() {
@@ -17,19 +18,26 @@ class App extends Component {
       <div>
 
         <center>
-          <div className="container-fluid">
-
-            <div className="row">
-              <button className="button"> Send </button>
-              <button className="button"> Recieve </button>
-            </div>
-            <div className="row">
-              <div className="col-sm-4">
+          <table className="table">
+            <div className="container-fluid">
+              <tr>
+                <div className="row">
+                  <td>
+                    <button className="button"> Send </button>
+                  </td>
+                  <td>
+                    <button className="button"> Recieve </button>
+                  </td>
+                </div>
+              </tr>
+              <div className="row">
+                <div className="col-sm-4">
+                </div>
+              </div>
+              <div >
               </div>
             </div>
-            <div >
-            </div>
-          </div>
+          </table>
         </center>
       </div>
     );
